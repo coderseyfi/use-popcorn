f19d0582a0c9da6ba72949de74066b39d6da6ee2
coderseyfi <seyfaddinmn@code.edu.az>
2024-01-14 16:04:29 +0400

    complete

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import './index.css'
+// import App from './App'
+
+import StarRating from './StarRating'
+import { useState } from 'react'
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} startsa</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={['terrible', 'bad', 'good', 'amazing', 'nice']}
+    />
+    <StarRating size={24} color="green" className="test" defaultRating={3} />
+    <Test />
+  </React.StrictMode>
+)
